c9ef1385e6e7609a0ee12f3bde3136b6f6e1eeea
rnoguer22 <rnogugon@myuax.com>
2023-11-10 13:09:41 +0100

    Adaptamos views.py para conectar arduino

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, HttpResponse
+import serial
 
 # Create your views here.
 
 def home(request):
+    ser = serial.Serial('COM4', 9600)  # Ajusta el puerto según tu configuración
+    ser.write(b'1')  # Envía un byte para encender la luz
+    ser.close()
     return render(request, 'core/index.html')
 
 def api(request):
+    ser = serial.Serial('COM4', 9600)  # Ajusta el puerto según tu configuración
+    ser.write(b'2')  # Envía un byte para encender la luz
+    ser.close()
     return render(request, 'core/api.html')
 
 def inicio(request):
+    ser = serial.Serial('COM4', 9600)  # Ajusta el puerto según tu configuración
+    ser.write(b'3')  # Envía un byte para encender la luz
+    ser.close()
     return render(request, 'core/inicio.html')
