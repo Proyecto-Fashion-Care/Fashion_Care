0f6eb6ceb9ed55dfe9297fb655a19293a129d053
rnoguer22 <rnogugon@myuax.com>
2023-11-14 19:04:57 +0100

    conexion con arduino

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,22 +1,43 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 import serial
+import time
 
 # Create your views here.
-
 def home(request):
-    ser = serial.Serial('COM4', 9600)  # Ajusta el puerto según tu configuración
-    ser.write(b'1')  # Envía un byte para encender la luz
-    ser.close()
-    return render(request, 'core/index.html')
+    try:
+        ser = serial.Serial('COM9', 9600)  # Ajusta el puerto según tu configuración
+    except:
+        print("No se pudo abrir el puerto serial")
+    else:
+        time.sleep(1)
+        ser.write(b'1')  # Envía un byte para encender la luz
+        print('portada')
+        ser.close()
+    finally:
+        return render(request, 'core/index.html')
 
 def api(request):
-    ser = serial.Serial('COM4', 9600)  # Ajusta el puerto según tu configuración
-    ser.write(b'2')  # Envía un byte para encender la luz
-    ser.close()
-    return render(request, 'core/api.html')
+    try:
+        ser = serial.Serial('COM9', 9600)  # Ajusta el puerto según tu configuración
+    except:
+        print("No se pudo abrir el puerto serial")
+    else:
+        time.sleep(1)
+        ser.write(b'2')  # Envía un byte para encender la luz
+        print('api')
+        ser.close()
+    finally:
+        return render(request, 'core/api.html')
 
 def inicio(request):
-    ser = serial.Serial('COM4', 9600)  # Ajusta el puerto según tu configuración
-    ser.write(b'3')  # Envía un byte para encender la luz
-    ser.close()
-    return render(request, 'core/inicio.html')
+    try:
+        ser = serial.Serial('COM9', 9600)  # Ajusta el puerto según tu configuración
+    except:
+        print("No se pudo abrir el puerto serial")
+    else:
+        time.sleep(1)
+        ser.write(b'3')  # Envía un byte para encender la luz
+        print('inicio')
+        ser.close()
+    finally:
+        return render(request, 'core/inicio.html')
