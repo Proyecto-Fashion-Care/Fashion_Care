dc04a051ae380695d5fbbb0cf1b63e62af192091
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2023-12-02 18:04:50 +0100

    subir

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core", #Agregamos la app core
+    "webpersonal.core", #Agregamos la app core
 ]
 
 MIDDLEWARE = [
