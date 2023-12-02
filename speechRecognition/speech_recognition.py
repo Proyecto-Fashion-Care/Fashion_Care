2544a734b3b0e1a0d34e4ab515fc3cea34232498
Pelayo <phuermij@myuax.com>
2023-12-02 14:15:31 +0100

    función para capturar audio

--- speechRecognition/speech_recognition.py
+++ speechRecognition/speech_recognition.py
@@ -3,3 +3,34 @@ import pyttsx3
 import time
 import threading
 
+def getAudio():
+    """
+    Función que captura el audio a través de un micrófono.
+    Devuelve un texto: str
+    """
+
+    #Creamos la instancia del reconocedor y del micrófono
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+
+    while True:
+        with mic as source:
+            print("Escuchando...")
+            r.pause_threshold = 2
+            r.adjust_for_ambient_noise(source)
+
+            try:
+                audio = r.listen(source, timeoff=1)
+                print("Procesando...")
+                text = r.recognize_google(audio, language="es-ES")
+                return text
+            except sr.UnknownValueError:
+                print("No he entendido lo que has dicho, por favor repítelo.")
+            except sr.RequestError as e:
+                print("Error en la solicitud: {e}".format(e))
+            except sr.WaitTimeoutError:
+                print("Time out.")
+
+            
+
+
