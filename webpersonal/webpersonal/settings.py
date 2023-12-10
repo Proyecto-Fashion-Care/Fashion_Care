87c15c2d1e62f18d491307e706c5853b7e87ada7
Pelayo <phuermij@myuax.com>
2023-12-10 22:40:05 +0100

    configuracion de settings.py

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b!fswk^@lf*%lm+_f9o#*oely=^=mpi1wp^nz_nu5v&b7d-^p*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 # Application definition
 
@@ -72,11 +73,16 @@ WSGI_APPLICATION = "webpersonal.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+#Base de datos postgresql (railway) porque vercel no permite sqlite3
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'D1cBBdCC1fE3ACcGd54gFF4G*bFGgfdb',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '26506',
     }
 }
 
@@ -122,3 +128,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = 'confirmacion'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
