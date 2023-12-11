0e1025bcc06af07df04d153b5432c0eff2f1735d
rnoguer22 <rnogugon@myuax.com>
2023-12-11 11:43:25 +0100

    Revert "Preparado el deploy en railway"
    This reverts commit f6281892041d16d3bdedcf6e2ec25a3a65732ae9.

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b!fswk^@lf*%lm+_f9o#*oely=^=mpi1wp^nz_nu5v&b7d-^p*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 # Application definition
 
@@ -122,19 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
-''' ¿?
-STATIC_ROOT = os.path.join(BASE_DIR, 'core/staticfiles/')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static/')
-]
-'''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = 'confirmacion'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
